b9fe664831fee40a0a787f83b4425763194f3f04
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-04-11 09:53:27 +0100

    s

--- functions.py
+++ functions.py
@@ -1,7 +1,25 @@
 import pandas as pd
+from functions import ProcessPoolExecutor, as_completed
+from abstract_sample import AbstractSample
 
 def remove_last_col(sample:pd.DataFrame)->pd.DataFrame:
     return sample.drop(sample.columns[-1], axis=1)
 
 def print_to_csv(sample:pd.DataFrame, file_name:str):
     sample.to_csv(file_name, index=False)
+
+# Function to process files using ProcessPoolExecutor  file_paths, dir:int = 0
+def process_files_with_processes(abstract_sample:AbstractSample):
+    samples = []
+    with ProcessPoolExecutor() as executor:
+        # Submit all the file paths for processing
+        tasks = [executor.submit(abstract_sample.extract_sample, path) for path in abstract_sample.paths]
+
+        for task in as_completed(tasks):
+            try:
+                # Retrieve the result and append it to the samples list
+                data = task.result()
+                samples.append(data)
+            except Exception as exc:
+                print(f'Generated an exception: {exc}')
+    return pd.concat(samples, ignore_index=True)
--- run.py
+++ run.py
@@ -1,24 +1,8 @@
 import pandas as pd
 import numpy as np
-from functions import remove_last_col, print_to_csv
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed, wait
-from abstract_sample import AbstractSample, Trip, Ride, Station
-
-# Function to process files using ProcessPoolExecutor  file_paths, dir:int = 0
-def process_files_with_processes(abstract_sample:AbstractSample):
-    samples = []
-    with ProcessPoolExecutor() as executor:
-        # Submit all the file paths for processing
-        tasks = [executor.submit(abstract_sample.extract_sample, path) for path in abstract_sample.paths]
-
-        for task in as_completed(tasks):
-            try:
-                # Retrieve the result and append it to the samples list
-                data = task.result()
-                samples.append(data)
-            except Exception as exc:
-                print(f'Generated an exception: {exc}')
-    return pd.concat(samples, ignore_index=True)
+from functions import remove_last_col, print_to_csv, process_files_with_processes
+from concurrent.futures import ThreadPoolExecutor, wait
+from abstract_sample import Trip, Ride, Station
     
 def main():
     print("Start Reading. The process might take up to 3 minute...")
